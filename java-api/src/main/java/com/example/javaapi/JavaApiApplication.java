3ab8d58bd7edee36da5fdd49a35163c11427451c
fabio <fabio.meihua08@gmail.com>
2020-08-15 19:21:14 -0300

    API Created

--- java-api/src/main/java/com/example/javaapi/JavaApiApplication.java
+++ java-api/src/main/java/com/example/javaapi/JavaApiApplication.java
@@ -2,12 +2,31 @@ package com.example.javaapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
+@RestController
 @SpringBootApplication
-public class JavaApiApplication {
+public class JavaApiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaApiApplication.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+		return builder.sources(JavaApiApplication.class);
+	}
+
+	@GetMapping("/")
+	public String ping(){
+		return "API ROCKS !! " + new Date().toString();
+	}
+
+
 
 }
---
+++ java-api/src/test/java/com/example/javaapi/JavaApiApplicationTest.java
@@ -0,0 +1,16 @@
+package com.example.javaapi;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JavaApiApplicationTest {
+
+    @Test
+    void configure() {
+    }
+
+    @Test
+    void ping() {
+    }
+}
